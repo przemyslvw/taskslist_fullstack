2ea0548bc37ab85b64790cf2365ec7ad7d1cebd3
Przemysław Kadjam <przemyslaw.kadjam@gmail.com>
2019-11-05 15:56:53 +0100

    clean operations code

--- frontend/src/reducers/tasks/operations.js
+++ frontend/src/reducers/tasks/operations.js
@@ -1,34 +1,34 @@
 import actions from "./actions";
 import axios from "axios";
 
-// nnew operations
+const baseURL = "http://localhost:4000/tasks";
+
 export const getTasks = () => async dispatch => {
-  axios
-    .get("http://localhost:4000/tasks/")
-    .then(response => {
-      dispatch(actions.getall(response.data));
-    })
-    .catch(error => {
-      console.log(error.response);
-    });
+  try {
+    const response = await axios.get(baseURL);
+    const data = await response.data;
+    dispatch(actions.getall(data));
+  } catch (error) {
+    console.log(error.response);
+  }
 };
 
 export const addTask = task => {
   axios
-    .post("http://localhost:4000/tasks/add", task)
+    .post(`${baseURL}/add`, task)
     .then(res => console.log(res.data))
     .catch(error => {
       console.log(error.response);
     });
 };
 
-export const getTask = taskId => async dispatch => {
+export const getTask = taskId => dispatch => {
   dispatch(actions.getTask(taskId));
 };
 
 export const saveTask = task => {
   axios
-    .post("http://localhost:4000/tasks/update/" + task._id, task)
+    .post(`${baseURL}/update/${task._id}`, task)
     .then(res => console.log(res.data))
     .catch(error => {
       console.log(error.response);
@@ -36,18 +36,18 @@ export const saveTask = task => {
 };
 
 export const startTask = task => {
-  let d = new Date();
+  const d = new Date();
   task.start = d;
   task.started = true;
   axios
-    .post("http://localhost:4000/tasks/update/" + task._id, task)
+    .post(`${baseURL}/update/${task._id}`, task)
     .then(res => console.log(res.data));
 };
 export const stopTask = task => {
-  let d = new Date();
+  const d = new Date();
   task.stop = d;
   task.visible = false;
   axios
-    .post("http://localhost:4000/tasks/update/" + task._id, task)
+    .post(`${baseURL}/update/${task._id}`, task)
     .then(res => console.log(res.data));
 };
